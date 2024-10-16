f1f9fe6642d4530e0a1fc45b0355e5039d434db6
Kelvin Ng <hoishing@gmail.com>
2024-10-16 10:32:43 +0800

    hide st menu

--- main.py
+++ main.py
@@ -9,4 +9,23 @@ synastry = st.Page("synastry.py", title="Synastry", icon=":material/group:")
 pg = st.navigation([birth, transit, synastry])
 st.set_page_config(**page_config)
 st.logo(image="static/astrobro-logo.png")
+# st.html(
+#     """<style>
+#         .stApp [data-testid="stToolbar"] {
+#             display: none;
+#         }
+#         </style>"""
+# )
+# st.html(
+#     """<style>
+#         #MainMenu {visibility: hidden;}
+#         footer {visibility: hidden;}
+#         </style>"""
+# )
+st.html(
+    """<style>
+        #MainMenu {display: none;}
+        footer {display: none;}
+        </style>"""
+)
 pg.run()
